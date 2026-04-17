c193ee559364a08f7beaf91c794897574f73fbb1
dev-harvey <harvrball@gmail.com>
2026-04-17 12:19:04 -0400

    Remove dist folder from version control

--- dist/phaser-shim.js
+++
@@ -1,4 +0,0 @@
-// Browser shim for importing 'phaser' as an ES module.
-// Assumes dist/phaser.js has been loaded as a non-module and set window.Phaser.
-const Phaser = (window && window.Phaser) ? window.Phaser : undefined;
-export default Phaser;
